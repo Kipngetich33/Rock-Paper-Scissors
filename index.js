1546022c039975e28b72019bf0a032febfc12fae
Kipngetich33 <khalifngeno@gmail.com>
2022-03-13 01:34:11 +0300

    add initial code to launch a react frontend

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppViews from './views/AppViews';
+import DeployerViews from './views/DeployerViews';
+import AttacherViews from '/views/AttacherViews';
+import { renderDOM, renderView } from './views/render';
+import './index.css';
+import * as backend from './build/index.main.mjs';
+import { loadStdlib } from '@reach-sh/stdlib';
+const reach = loadStdlib(process.env)
+
+const handToInt = {'ROCK':0, 'PAPER':1, 'SCISSORS':2};
+const intToOutcome = ['Bob wins!', 'Draw!', 'Alice wins!'];
+const { standardUnit } = reach;
+const defaults = {defaultFundAmt: '10', defaultWager: '3', }
+
